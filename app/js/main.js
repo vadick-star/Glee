6bb8e60d844e1ca57b1bdc2fff6f90719aa016c6
vadick-star <hel-333@mail.ru>
2021-03-13 17:36:26 +0300

    added slider(section-smart) and mixitup(section-products)

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-  
+  $('.smart__slider-wrapper').slick({
+    arrows: false,
+    dots: true
+  });
+
+  var mixer = mixitup('.products__items');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,8 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'app/js/slick.min.js',
+    'app/js/mixitup.min.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
