ee853669940e7ce146d6bbf5e2d293f760bc1152
vadick-star <hel-333@mail.ru>
2021-03-15 14:53:02 +0300

    fix slider and mixitup

--- gulpfile.js
+++ gulpfile.js
@@ -32,8 +32,8 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'app/js/slick.min.js',
-    'app/js/mixitup.min.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
